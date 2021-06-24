45ce24f72003c133a79d5abb1c439e8e395bae06
Mohammed Idris <midris360@gmail.com>
2021-06-23 21:26:54 -0400

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of The Matrix franchise. He was portrayed as a cybercriminal and computer programmer by Keanu Reeves in The Matrix Trilogy, as well as having a cameo in The Animatrix short film Kid's Story. Andrew Bowen provided Neo's voice in The Matrix: Path of Neo. In 2008, Neo was selected by Empire as the 68th Greatest Movie Character of All Time.[1] Neo is also an anagram of "one", a reference to his destiny of being The One who would bring peace.[2] There are claims that a nightclub in Chicago inspired the name of the character.[3][4] Neo is considered to be a superhero. },
+	{ name: "Trinity", description: Morpheus is a fictional character in The Matrix franchise.[1] He is portrayed by Laurence Fishburne in the films, and in the video game The Matrix: Path of Neo where he was the only actor to reprise his character's voice.},
+	{ name: "Morpheus", description: Morpheus is that of the god of dreams in Greek mythology, which is consistent with the characters involvement with the dreaming of the Matrix. },
+	{ name: "Agent Smith", description: Agent Smith began as an agent, an AI program in the Matrix programmed to keep order within the system by terminating human simulacra. },
 ])
